506ce2bc2d735b73ef49c9c167688820453f91af
SquashyShrub <joshuacrandall2003@gmail.com>
2026-05-20 10:32:03 -0700

    Created text area for weak prompt. Optimized page layout for one call to action.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { LandingHeading } from "@/components/LandingHeading";
+import { WeakPromptTextArea } from "@/components/WeakPromptTextArea";
 
 export default function Home() {
   return (
     <main>
       <LandingHeading />
+      <WeakPromptTextArea />
     </main>
   );
 }
